d5dca12a90daa37b5cbcb522e4b94f35a6f2ab08
TheJoma <mateo_06_jse_98@hotmail.com>
2022-10-27 18:11:25 -0500

    crear el repositorio de producto y las clases de servicio y el metodo show de productocontroller

---
+++ src/main/java/com/ecommerce/ecommerce/repository/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.ecommerce.ecommerce.repository;
+
+import com.ecommerce.ecommerce.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto,Long> {
+}
---
+++ src/main/java/com/ecommerce/ecommerce/service/IProductoService.java
@@ -0,0 +1,21 @@
+package com.ecommerce.ecommerce.service;
+
+import com.ecommerce.ecommerce.model.Producto;
+
+import java.util.Optional;
+
+public interface IProductoService {
+    //guardar
+    public Producto save(Producto producto);
+
+    //para obtener un objeto de la base de datos si retorna null por eso es el optional
+    public Optional<Producto> get(Long id);
+
+    //actualizar producto
+    public void update(Producto producto);
+
+    //Eliminar Producto
+    public void delete(Long id);
+
+
+}
