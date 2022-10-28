f31735912c6042e29f694a1e4bcdffb01f99a85b
TheJoma <mateo_06_jse_98@hotmail.com>
2022-10-28 15:53:19 -0500

    funcionalidad para editar productos

--- src/main/java/com/ecommerce/ecommerce/controller/ProductoController.java
+++ src/main/java/com/ecommerce/ecommerce/controller/ProductoController.java
@@ -7,10 +7,9 @@ import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/productos")
@@ -22,17 +21,19 @@ public class ProductoController {
     @Autowired
     private ProductoServiceImpl productoService;
 
+    //vista para la lista de productos
     @GetMapping("")
     public String show(Model model){
         model.addAttribute("productos",productoService.findAll());
         return "producto/show";
     }
 
+    //ir a la vista de registrar producto
     @GetMapping("/create")
     public String create(){
         return "producto/create";
     }
-
+   //metodo para registrar el producto
     @PostMapping("/save")
     public String save(Producto producto){
         LOGGER.info("Este es el objeto de producto {}",producto);
@@ -41,5 +42,27 @@ public class ProductoController {
         productoService.save(producto);
         return "redirect:/productos";
     }
+
+    // obtiene los datos del producto y los muestra en la vista de actualizar
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Long id, Model model){
+        Producto producto = new Producto();
+        Optional<Producto> optionalProducto = productoService.get(id);
+        producto = optionalProducto.get();
+
+        LOGGER.info("producto buscando : {}",producto);
+        model.addAttribute("producto",producto);
+       return "producto/edit";
+    }
+
+    //actualizar y dirigir a la vista de show
+    @PostMapping("/update")
+    public String update(Producto producto){
+        productoService.update(producto);
+        return "redirect:/productos";
+    }
+
+
+
 
 }
