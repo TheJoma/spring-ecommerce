900f9afd39bff9bd6d4fc72073b24b61d33b826b
TheJoma <mateo_06_jse_98@hotmail.com>
2022-11-02 13:51:59 -0500

    añadir validacion para doble producto y ver el carrito desde la cabecera del template del usuario opcion menu

--- src/main/java/com/ecommerce/ecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/ecommerce/controller/HomeController.java
@@ -69,7 +69,13 @@ public class HomeController {
         detalleOrden.setTotal(producto.getPrecio()*cantidad);
         detalleOrden.setProducto(producto);
 
-        detalles.add(detalleOrden);
+        //validar que el producto no se añada 2 veces
+        Long idProducto = producto.getId();
+        boolean ingresado =detalles.stream().anyMatch(p -> p.getProducto().getId()==idProducto);
+
+        if(!ingresado){
+            detalles.add(detalleOrden);
+        }
 
         sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
 
@@ -80,4 +86,38 @@ public class HomeController {
 
         return "usuario/carrito";
     }
+
+
+    //quitar un producto del carrito
+    @GetMapping("/delete/cart/{id}")
+    public String deleteProductoCart(@PathVariable Long id, Model model){
+        //lista nueva de productos
+        List<DetalleOrden> ordenesNueva = new ArrayList<DetalleOrden>();
+
+        for(DetalleOrden detalleOrden : detalles){
+            if(detalleOrden.getProducto().getId()!=id){
+                ordenesNueva.add(detalleOrden);
+            }
+        }
+
+        //poner la nueva lista con los productos restantes
+        detalles=ordenesNueva;
+
+        double sumaTotal = 0;
+        sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
+
+        orden.setTotal(sumaTotal);
+        model.addAttribute("cart",detalles);
+        model.addAttribute("orden",orden);
+
+        return "usuario/carrito";
+    }
+
+    @GetMapping("/getCart")
+    public String getCart(Model model){
+        model.addAttribute("cart",detalles);
+        model.addAttribute("orden",orden);
+        return "/usuario/carrito";
+    }
+
 }
